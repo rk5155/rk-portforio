4f53b43367031227bdcb287eb6879adb55837485
rk <>
2021-11-16 11:36:58 +0900

    メインビジュアル部分を作成した

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <Header></Header>
+    <MainVisual></MainVisual>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
+import MainVisual from './components/MainVisual.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    Header
+    Header,
+    MainVisual
   }
 }
 </script>
---
+++ src/components/MainVisual.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="main-visual">
+        <div>
+            <h1>Ryu-ki Production</h1><br>
+            <p>ワンランク上のWEB制作を。</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#main-visual {
+    height: 986px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+#main-visual h1 {
+    
+}
+</style>
