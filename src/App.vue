49c2a4ec3cb3077395454f91d2aeae18e924fc30
rk <>
2021-11-16 15:14:44 +0900

    Content1 コンポーネントを作成し、Content1でプロフィールを作成した

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
     <Header></Header>
+
     <MainVisual></MainVisual>
+
     <Title title="プロフィール" en="profile" name="fas fa-user"></Title>
+
+    <Content1 img='/img/IMG_4933.f3421192.jpg'>
+        <div>
+            <h3>Ryu-ki</h3>
+
+            <p>フリーランスエンジニア</p>
+
+            <p>大学卒業後アパレル会社にて勤務▶︎IT企業に転職▶︎フリーランス独立</p>
+
+            <p>Vue.jsを使用した、高速なWEBサイト制作が得意<br>顧客を待たせない、高速なサイトをお作りいたします。</p>
+
+            <a href="">詳しい経歴プロフィールはこちら</a>
+
+        </div>
+    </Content1>
   </div>
 </template>
 
@@ -11,6 +28,7 @@
 import Header from './components/Header.vue'
 import MainVisual from './components/MainVisual.vue'
 import Title from './components/Title.vue'
+import Content1 from './components/Content1.vue'
 
 export default {
   name: 'App',
@@ -18,7 +36,8 @@ export default {
     // HelloWorld
     Header,
     MainVisual,
-    Title
+    Title,
+    Content1
   }
 }
 </script>
---
+++ src/components/Content1.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="content1">
+        <div>
+            <img :src="img" alt="">
+        </div>
+
+        <slot></slot>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'text', 'linkText', 'url', 'img'],
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+.content1 {
+    width: 980px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    padding: 50px 0;
+}
+.content1 img {
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+}
+.content1 a {
+    margin-top: 20px;
+    border: 2px solid #42B983;
+    display: inline-block;
+    padding: 10px;
+    color: #212529;
+}
+</style>
