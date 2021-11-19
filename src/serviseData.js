d77599bc7c171c633b672338e0e078e5a18d315c
rk <>
2021-11-19 15:19:04 +0900

    mixin を使ってサービス内容のデータを共有化した

---
+++ src/serviseData.js
@@ -0,0 +1,21 @@
+export const serviseData = {
+    data: function() {
+        return {
+          servises: [
+                        { id: 1, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--red' },
+                        { id: 2, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--perple'  },
+                        { id: 3, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--yellow'  },
+                        { id: 4, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--pink'  }
+                    ],
+          prices: [
+                        { id: 1, name: "aaa", price: '200,000円〜' },
+                        { id: 2, name: 'aaa', price: '200,000円〜' },
+                        { id: 3, name: 'aaa', price: '200,000円〜' },
+                        { id: 4, name: 'aaa', price: '200,000円〜' },
+                        { id: 5, name: 'aaa', price: '200,000円〜' },
+                        { id: 6, name: 'aaa', price: '200,000円〜' },
+                        { id: 7, name: 'aaa', price: '200,000円〜' },
+                     ]
+        }
+    }
+}
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -7,30 +7,15 @@
 
 <script>
 import Content2 from '../components/Content2.vue'
+// @ は省略記法 src 配下を省略できる
+// import { serviseData } from '../serviseData.js' と同じ
+import { serviseData } from '@/serviseData.js'
+
 
 export default {
   components: {
     Content2
-    
   },
-  data: function() {
-    return {
-      servises: [
-                    { id: 1, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--red' },
-                    { id: 2, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--perple'  },
-                    { id: 3, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--yellow'  },
-                    { id: 4, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--pink'  }
-                ],
-      prices: [
-                    { id: 1, name: "aaa", price: '200,000円〜' },
-                    { id: 2, name: 'aaa', price: '200,000円〜' },
-                    { id: 3, name: 'aaa', price: '200,000円〜' },
-                    { id: 4, name: 'aaa', price: '200,000円〜' },
-                    { id: 5, name: 'aaa', price: '200,000円〜' },
-                    { id: 6, name: 'aaa', price: '200,000円〜' },
-                    { id: 7, name: 'aaa', price: '200,000円〜' },
-                 ]
-    }
-  }
+  mixins: [ serviseData ]
 }
 </script>
