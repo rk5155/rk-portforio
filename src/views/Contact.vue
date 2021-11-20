13e2702843095095854ef12e1e55f92e2dcbb399
rk <>
2021-11-20 14:36:46 +0900

    google forms でお問い合わせフォームを設置した

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,3 +1,13 @@
 <template>
-    <h2>contact</h2>
+    <div id="contact">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdFkeSk_uWl5WRGv8Bn4DFf9YS9DxpfjBEVCSffHTd4jYiH5Q/viewform?embedded=true" width="640" height="962" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">読み込んでいます…</iframe>
+    </div>
+    
 </template>
+
+<style>
+#contact {
+    margin: 70px 0;
+    text-align: center;
+}
+</style>
