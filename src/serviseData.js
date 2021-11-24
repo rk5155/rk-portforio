862cb29bde1cd60bbea09025ae6bbb3d65dd3cd3
rk <>
2021-11-24 12:35:38 +0900

    サービス内容のテキストを変更した

--- src/serviseData.js
+++ src/serviseData.js
@@ -2,10 +2,10 @@ export const serviseData = {
     data: function() {
         return {
           servises: [
-                        { id: 1, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--red', url: '/service/1'},
-                        { id: 2, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--perple', url: '/service/2' },
-                        { id: 3, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--yellow', url: '/service/3'},
-                        { id: 4, h3: "Webサイト制作・カスタマイズ", p: 'テキストテキストテキストテキスト', list1: 'テキスト', list2: 'テキスト', list3: 'テキストテキスト', list4: 'テキスト', class: 'content2__title--pink', url: '/service/4' }
+                        { id: 1, h3: "WEBサイト制作・カスタマイズ", p: 'シンプルなWEBサイト制作から、SEO対策までトータルサポートが可能です。', list1: 'SEO対策', list2: 'アニメーション', list3: 'レスポンシブ対応', list4: 'お問い合わせフォーム', class: 'content2__title--red', url: '/service/1'},
+                        { id: 2, h3: "LP制作", p: 'デザインからHTML、CSSコーディング、レスポンシブ対応までトータルサポートが可能です。', list1: 'SEO対策', list2: 'アニメーション', list3: 'レスポンシブ対応', list4: 'お問い合わせフォーム', class: 'content2__title--perple', url: '/service/2' },
+                        { id: 3, h3: "サイトの保守管理", p: 'サイトのアップデートやメンテナンス、セキュリティー対策、SEOの改善など、サイト運用に関することをサポートいたします。', list1: 'メンテナンス', list2: 'アップデート', list3: 'セキュリティー対策', list4: 'データを守るバックアップ', class: 'content2__title--yellow', url: '/service/3'},
+                        { id: 4, h3: "SPA(シングルページアプリケーション)制作", p: '顧客を待たせない、高速なWEBサイトを作成いたします。', list1: 'SEO対策', list2: 'アニメーション', list3: 'レスポンシブ対応', list4: 'お問い合わせフォーム', class: 'content2__title--pink', url: '/service/4' }
                     ],
           prices: [
                         { id: 1, name: "aaa", price: '200,000円〜' },
