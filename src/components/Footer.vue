b3669f86adc9a87edd202350b3b3916cdf430482
rk <>
2021-11-25 11:07:11 +0900

    footer のデザインを変更した

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,101 @@
 <template>
-    <footer>
-        <p>Ryu-ki Production</p>
+    <footer class="text-center text-white">
+        <!-- Grid container -->
+        <div class="container">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+            <!-- Facebook -->
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #3b5998;"
+                href="#!"
+                role="button"
+                ><i class="fab fa-facebook-f"></i
+            ></a>
 
-        <p>kjsdfkljlkjsakdl</p>
+            <!-- Twitter -->
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #55acee;"
+                href="#!"
+                role="button"
+                ><i class="fab fa-twitter"></i
+            ></a>
+
+            <!-- Google -->
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #dd4b39;"
+                href="#!"
+                role="button"
+                ><i class="fab fa-google"></i
+            ></a>
+
+            <!-- Instagram -->
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #ac2bac;"
+                href="#!"
+                role="button"
+                ><i class="fab fa-instagram"></i
+            ></a>
+
+            <!-- Linkedin -->
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #0082ca;"
+                href="#!"
+                role="button"
+                ><i class="fab fa-linkedin-in"></i
+            ></a>
+            <!-- Github -->
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #333333;"
+                href="#!"
+                role="button"
+                ><i class="fab fa-github"></i
+            ></a>
+            </section>
+            <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © {{ reversedYear }} Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">Ryu-ki Production. All rights reserved.</a>
+        </div>
+        <!-- Copyright -->
     </footer>
 </template>
+
+<script>
+export default {
+  computed: {
+    reversedYear: function () {
+        let today = new Date();
+        let year = today.getFullYear();
+        return year
+    }
+  }
+}
+</script>
 
 <style>
 footer {
     background-color: #3B4552;
     color: #ffffff;
-    height: 500px;
-    padding: 50px 30px;
+
 }
 footer p {
     margin: 0;
+}
+.btn-primary  {
+    border-color: #3B4552;
+}
+footer .container {
+    margin: 0 auto;
+    padding: 80px 0 70px;
 }
 </style>
